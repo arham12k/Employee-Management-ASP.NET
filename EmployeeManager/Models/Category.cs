7f67efd1844e31ab27397cf54e1c2be4faf991ff
arham12k <khanarham8962@gmail.com>
2024-08-05 18:00:02 +0530

    Add category model in the models folder

---
+++ EmployeeManager/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace EmployeeManager.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set;}
+        [Required]
+        public string Name { get; set;}
+        public string DisplayOrder { get; set; }
+    }
+}
