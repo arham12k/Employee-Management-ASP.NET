94217ff7a176743b6eb49f6fceac2012e6c30fbe
arham12k <khanarham8962@gmail.com>
2024-08-05 20:22:56 +0530

    AddDbContext service in the Program.cs using connection string

---
+++ EmployeeManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
