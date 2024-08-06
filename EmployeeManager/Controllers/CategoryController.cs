5507b135e6c98f0e9afea383ab989360bd32d828
arham12k <khanarham8962@gmail.com>
2024-08-06 13:08:04 +0530

    Retrieve data from database using category controller constructor

--- EmployeeManager/Controllers/CategoryController.cs
+++ EmployeeManager/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeeManager.Data;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManager.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db=db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList= _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
